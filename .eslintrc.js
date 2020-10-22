06d1442561ffbc5ed37c9571d8da079aa114338e
bydongxin <bydongxin@gmail.com>
2020-10-22 23:31:22 +0800

    兼容rem

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'semi':  0, //结尾分号验证关闭
+        "indent": ["off", 2],//两个空格验证关闭
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
 }
